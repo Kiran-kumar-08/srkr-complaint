46deb5f2cd1e6763d9f59c7033d3e661e3ae5377
PanduNamburi <pandunamburi18@gmail.com>
2025-09-10 17:02:22 +0530

    Updated API baseURL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://srkrcomplaint.onrender.com/api',
+  baseURL: 'https://srkrcomplaint.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
